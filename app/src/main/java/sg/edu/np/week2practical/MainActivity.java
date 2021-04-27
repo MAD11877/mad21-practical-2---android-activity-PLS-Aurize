28e8a81f2d702be8e402016ee87eeefc521c0faa
PLS_Aurize <s10184988@connect.np.edu.sg>
2021-04-27 21:45:34 +0800

    Week 2 practical

---
+++ app/src/main/java/sg/edu/np/week2practical/MainActivity.java
@@ -0,0 +1,41 @@
+package sg.edu.np.week2practical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "Main Activity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v(TAG, "On Create!");
+        User user = new User("Reiner", "Student", 001, false );
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.isFollowed() == false){
+                    button.setText("Unfollow");
+                    user.setFollowed(true);
+                }
+                else{
+                    button.setText("Follow");
+                    user.setFollowed(false);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+}
